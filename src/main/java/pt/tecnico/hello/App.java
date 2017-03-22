8e5811ac599787a0c1d981c62e3224a7146081b2
seniwar <a.seniwar@gmail.com>
2017-03-22 17:26:58 +0000

    aa

--- src/main/java/pt/tecnico/hello/App.java
+++ src/main/java/pt/tecnico/hello/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "ola!" );
-        System.out.println( "Hello World bla bla!" );
+        System.out.println( "Hello Worjbjvhjld bla bla!" );
     }
 }
